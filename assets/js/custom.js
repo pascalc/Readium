db16b1abb9a6d22223e84473612e5813b9dc35a5
Pascal Chatterjee <pascal.chatterjee@gmail.com>
2014-01-12 18:09:56 +0100

    Removed fb meta tags

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,10 +3,6 @@
     "use strict";
 
     $(document).ready(function(){
-      // Move meta tags from post body to head
-      $(".contentpost__inner meta[property]").appendTo("head");
-      // Remove meta tags from body
-      $(".contentpost__inner meta[property]").remove();
     });
 
 }(jQuery));
